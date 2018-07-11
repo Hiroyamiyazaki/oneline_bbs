8425498fff3e0bd747be2b625141c6a4ba5f406d
宮崎裕也 <sawara.seishin.sophia@gmail.com>
2018-07-11 09:05:52 +0800

    データベースを送信できるように変更

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,5 +1,29 @@
 <?php
   // ここにDBに登録する処理を記述する
+  if(isset($_POST) && !empty($_POST['nickname']) && !empty($_POST['comment'])){
+    $nickname = htmlspecialchars($_POST['nickname']);
+    $comment = htmlspecialchars($_POST['comment']);
+
+    if (empty($nickname)){
+      return;
+    };
+    if (empty($comment)){
+      return;
+    };
+
+    $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+
+    $sql = 'INSERT INTO `posts`(`nickname`,`comment`) VALUES ("'.$nickname.'","'.$comment.'")';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh = null;
+  };
 
 ?>
 <!DOCTYPE html>
@@ -9,7 +33,7 @@
   <title>セブ掲示版</title>
 </head>
 <body>
-    <form method="post" action="">
+    <form method="post" action="bbs_no_css.php">
       <p><input type="text" name="nickname" placeholder="nickname"></p>
       <p><textarea type="text" name="comment" placeholder="comment"></textarea></p>
       <p><button type="submit" >つぶやく</button></p>
