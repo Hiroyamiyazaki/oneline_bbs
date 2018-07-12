183dad2525fcf9281ec98d14f06f13ad521ba3e2
宮崎裕也 <sawara.seishin.sophia@gmail.com>
2018-07-12 21:40:53 +0800

    コミット

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,4 +1,10 @@
 <?php
+
+    $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
   // ここにDBに登録する処理を記述する
   if(isset($_POST) && !empty($_POST['nickname']) && !empty($_POST['comment'])){
     $nickname = htmlspecialchars($_POST['nickname']);
@@ -11,12 +17,6 @@
       return;
     };
 
-    $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
-    $user = 'root';
-    $password = '';
-    $dbh = new PDO($dsn, $user, $password);
-    $dbh->query('SET NAMES utf8');
-
 
     $sql = 'INSERT INTO `posts`(`nickname`,`comment`,`created`) VALUES (?,?,?)';
     $data [] =$nickname;
@@ -25,9 +25,29 @@
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
+  }
+
+    $sql ='SELECT * FROM `posts` ORDER BY `created` DESC';
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+
+    while (1) {
+      $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+      if($rec === false){
+          break;
+      }
+       echo $rec['nickname'] .'<br>';
+       echo $rec['comment'] .'<br>';
+       echo $rec['created'] .'<br>';
+       echo '<br>';
+    
+      # code...
+    }
 
     $dbh = null;
-  };
+  
 
 ?>
 <!DOCTYPE html>
@@ -43,6 +63,8 @@
       <p><button type="submit" >つぶやく</button></p>
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
+
+    
 
 </body>
 </html>
