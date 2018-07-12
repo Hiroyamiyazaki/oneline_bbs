4d176a2f7cbb772b3b66aad386615e4ed63dc8bb
宮崎裕也 <sawara.seishin.sophia@gmail.com>
2018-07-12 21:41:17 +0800

    コミット

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+
+//削除する処理
+
+    $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
+
+    $id =$_GET['id'];
+
+
+    $sql ='DELETE FROM `posts` WHERE `id` = ?';
+    $data[]=$_GET['id'];
+
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+
+
+     $dbh = null;
+// リダイレクト
+
+
+header("Location: bbs.php");
+exit();
